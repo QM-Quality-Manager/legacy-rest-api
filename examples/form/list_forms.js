995fe3f16dac948dd0b44adca233fd70dd17edd4
Christian Amor Kvalheim <christkv@gmail.com>
2020-01-25 18:26:10 +0100

    Added formlist function and model

---
+++ examples/form/list_forms.js
@@ -0,0 +1,29 @@
+const { FormService } = require('../../lib/services');
+const fs = require('fs');
+
+// const endPoint = "http://localhost:8080/qmplus/rest";
+const endPoint = null;
+
+// {
+//   "authTokenKey": "<your token>",
+//   "tenant": "<tenant db>",
+//   "username": "<your username>"
+//   "password": "<your password>"
+// }
+// Read the token
+const token = JSON.parse(fs.readFileSync(`${__dirname}/../../token.json`, `utf8`));
+const authTokenKey = token.authTokenKey;
+const tenant = token.tenant;
+const languageId = token.languageId;
+
+(async () => {
+  // Create user service
+  let service = new FormService(endPoint);
+  // Execute the service
+  let result = await service.formList(authTokenKey, tenant, languageId, languageId);
+  // Print the returned results
+  console.log(JSON.stringify(result, null, 2))
+})().then(() => {}).catch(err => {
+  console.dir(err);
+  process.exit(0);
+})
